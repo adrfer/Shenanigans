6cdb7b7d450358e9795331734ccb1684b6f1d686
Adriano Ferreira <adriano.ferreira@me.com>
2016-01-30 18:57:23 -0500

    Initial commit

---
+++ Shenanigans/Shenanigans.h
@@ -0,0 +1,19 @@
+//
+//  Shenanigans.h
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-01-30.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shenanigans.
+FOUNDATION_EXPORT double ShenanigansVersionNumber;
+
+//! Project version string for Shenanigans.
+FOUNDATION_EXPORT const unsigned char ShenanigansVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shenanigans/PublicHeader.h>
+
+
