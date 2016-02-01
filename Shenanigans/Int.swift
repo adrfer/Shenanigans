f3ca2763d83a70bc4c36e358949cfd2a3babb01c
Adriano Ferreira <adriano.ferreira@me.com>
2016-01-31 19:32:52 -0500

    Add `isEven` property to Int extension

---
+++ Shenanigans/Int.swift
@@ -0,0 +1,22 @@
+//
+//  Int.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-01-31.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+
+    // MARK: - Instance Properties
+    
+    /// Check whether an integer is even
+    ///
+    /// - returns: True if even, false otherwise
+
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+}
---
+++ ShenanigansTests/IntTests.swift
@@ -0,0 +1,9 @@
+//
+//  IntTests.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-01-31.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
