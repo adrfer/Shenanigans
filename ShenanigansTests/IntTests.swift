24643a21c62541c3a61f10b227095a5a40eb50e8
Adriano Ferreira <adriano.ferreira@me.com>
2016-01-31 19:36:10 -0500

    Add `isEven` property tests to Int extension

--- ShenanigansTests/IntTests.swift
+++ ShenanigansTests/IntTests.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+import XCTest
+@testable import Shenanigans
+
+class ShenanigansTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func testIsEven() {
+        for int in [-2, 0, 2, 4] { XCTAssertTrue(int.isEven) }
+        for int in [-1, 3, 5, 7] { XCTAssertFalse(int.isEven) }
+    }
+}
