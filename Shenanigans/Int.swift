2338697515b876cf5715ab67a3512d7bb105c631
Adriano Ferreira <adriano.ferreira@me.com>
2016-02-01 20:16:52 -0500

    Add `random` type method to Int extension

--- Shenanigans/Int.swift
+++ Shenanigans/Int.swift
@@ -27,4 +27,19 @@ public extension Int {
     var isOdd: Bool {
         return !isEven
     }
+
+    // MARK: - Type Methods
+
+    /// Generate a uniformly distributed random integer
+    ///
+    /// - parameters:
+    ///
+    ///   - lower: Inclusive lower bound random value
+    ///   - upper: Inclusive upper bound random value
+    ///
+    /// - returns: A random integer between lower and upper bounds
+
+    static func random(from lower: Int = 0, to upper: Int) -> Int {
+        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
 }
