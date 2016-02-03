7086ed4a0ecb14ff6d76a9d06964e75a4151a1d3
Adriano Ferreira <adriano.ferreira@me.com>
2016-02-03 16:35:57 -0500

    Add `toggle` instance method to Bool extension

---
+++ Shenanigans/Bool.swift
@@ -0,0 +1,24 @@
+//
+//  Bool.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-02-03.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
+
+
+public extension Bool {
+
+    // MARK: - Instance Methods
+
+    /// Toggle a bool value
+    ///
+    /// - returns: A toggled bool
+    
+    mutating func toggle() -> Bool {
+        self = !self
+        return self
+    }
+}
