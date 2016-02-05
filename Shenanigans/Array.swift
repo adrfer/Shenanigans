53292297bbe50dbb21e8e689c4cf2e083ccc4ff9
Adriano Ferreira <adriano.ferreira@me.com>
2016-02-04 20:48:37 -0500

    Added `head` instance property to Array extension

---
+++ Shenanigans/Array.swift
@@ -0,0 +1,22 @@
+//
+//  Array.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-02-04.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+
+    // MARK: - Instance Properties
+
+    /// Decompose `self` into its head
+    ///
+    /// - returns: The first element of `self` iff it is not empty, nil otherwise
+
+    var head: Element? {
+        return count > 0 ? first! : nil
+    }
+}
---
+++ ShenanigansTests/ArrayTests.swift
@@ -0,0 +1,9 @@
+//
+//  ArrayTests.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-02-04.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
