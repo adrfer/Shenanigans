8a2492218b4c8e9d3818ce957f82de0a61c7f855
Adriano Ferreira <adriano.ferreira@me.com>
2016-03-09 08:44:37 -0500

    Add `frequencies` method tests to SequenceType extension

---
+++ ShenanigansTests/SequenceTypeTests.swift
@@ -0,0 +1,20 @@
+//
+//  SequenceTypeTests.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-03-09.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import Shenanigans
+import Swift
+class SequenceTypeTests: XCTestCase {
+
+    func testFrequencies() {
+
+        let array = [1, 1, 2, 3, 3, 7, 7, 7]
+        XCTAssertEqual([1: 2, 2: 1, 3: 2, 7: 3], array.frequencies())
+    }
+}
