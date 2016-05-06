195a7521d40b882a7e259ecd1667c5aa2c26bd0a
Adriano Ferreira <adriano.ferreira@me.com>
2016-05-06 11:18:36 -0400

    Add MutableCollectionTypeTests file

---
+++ Tests/MutableCollectionTypeTests.swift
@@ -0,0 +1,31 @@
+//
+//  IntTests.swift
+//  Shenanigans
+//
+//  Created by Adriano Ferreira on 2016-05-06.
+//  Copyright © 2016 Adriano Ferreira. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable
+import Shenanigans
+
+class MutableCollectionType: XCTestCase {
+
+    func testShuffleInPlace() {
+        var first = Array(-7...7)
+        let second = first
+
+        first.shuffleInPlace()
+
+        XCTAssertEqual(first.count, second.count)
+
+        for value in second {
+            XCTAssertTrue(first.contains(value))
+        }
+
+        XCTAssertEqual(first.sort(), second.sort())
+    }
+}
