e66a9c3eb11e538db727eed8228cca47ee210628
Adriano Ferreira <adriano.ferreira@me.com>
2016-07-20 09:30:53 -0400

    Remove blank line between comments and code from Bool extension

--- Sources/Bool.swift
+++ Sources/Bool.swift
@@ -13,7 +13,6 @@ public extension Bool {
     // MARK: - Instance Methods
 
     /// Toggle `self` by mutating it
-
     mutating func toggle() {
 
         self = !self
@@ -22,7 +21,6 @@ public extension Bool {
     /// Toggle `self`
     ///
     /// - Returns: A toggled `self`
-    
     @discardableResult
     func toggled() -> Bool {
 
@@ -37,7 +35,6 @@ public extension Bool {
     /// Generate a uniformly distributed random bool
     ///
     /// - Returns: A random bool value
-
     @discardableResult
     static func random() -> Bool {
 
