e43207c16a4503bc63f181449ff0a2b7d9614e54
Adriano Ferreira <adriano.ferreira@me.com>
2016-11-09 09:40:37 -0500

    Update int extension and tests to swift 3

--- Sources/Int.swift
+++ Sources/Int.swift
@@ -9,119 +9,122 @@
 import Foundation
 
 public extension Int {
-
+    
     // MARK: - Instance Properties
     
     /// Check whether `self` is even
     ///
     /// - Returns: True iff `self` is even, false otherwise
+    
     var isEven: Bool {
-
         return self % 2 == 0
     }
-
+    
     /// Check whether `self` is odd
     ///
     /// - Returns: True iff `self` is odd, false otherwise
+    
     var isOdd: Bool {
-
         return !isEven
     }
-
+    
     /// Check whether `self` is positive
     ///
     /// - Returns: True iff `self` is greater than zero, false otherwise
+    
     var isPositive: Bool {
-
         return self > 0
     }
-
+    
     /// Check whether `self` is negative
     ///
     /// - Returns: True iff `self` is less than zero, false otherwise
+    
     var isNegative: Bool {
-
         return self < 0
     }
-
+    
     /// Check whether `self` is non-positive
     ///
     /// - Returns: True iff `self` is less than or equal to zero, false otherwise
+    
     var isNonPositive: Bool {
-
         return self <= 0
     }
-
+    
     /// Check whether `self` is non-negative
     ///
     /// - Returns: True iff `self` is greater than or equal to zero, false otherwise
+    
     var isNonNegative: Bool {
-
         return self >= 0
     }
-
+    
     /// Check whether `self` is non-zero
     ///
     /// - Returns: True iff `self` is not zero, false otherwise
+    
     var isNonZero: Bool {
-
         return self != 0
     }
-
+    
     // MARK: - Instance Methods
-
+    
     /// Repeat a block of code `self` times
     ///
     /// - Parameter closure: The code to be executed `self` times
+    
     func times(_ closure: () -> Void) {
-
+        
         guard self > 0 else {
             return
         }
-
+        
         for _ in 0 ..< self {
             closure()
         }
     }
-
+    
     /// Repeat a block of code `self` times
     ///
     /// - Parameter closure: The code to be executed `self` times
     func times(_ closure: (Int) -> Void) {
-
+        
         guard self > 0 else {
             return
         }
-
+        
         for index in 0 ..< self {
             closure(index)
         }
     }
-
+    
     /// Repeat a block of code from `self` up to a limit
     ///
     /// - Parameters:
     ///
-    ///   - upper: Non-inclusive upper bound value
+    ///   - upperBound: Non-inclusive upper bound value
     ///   - step: The step used to stride over a half-open interval
     ///   - closure: The block of code to be executed
-    func up(to upper: Int, by step: Int = 1, _ closure: () -> Void) {
-
-        for _ in Swift.stride(from: self, to: upper, by: step) {
+    
+    func up(to upperBound: Int, by step: Int = 1, _ closure: () -> Void) {
+        
+        for _ in Swift.stride(from: self, to: upperBound, by: step) {
             closure()
         }
     }
-
+    
     /// Repeat a block of code from `self` down to a limit
     ///
     /// - Parameters:
     ///
-    ///   - lower: Inclusive lower bound value
+    ///   - lowerBound: Inclusive lower bound value
     ///   - step: The step used to stride over the closed interval
     ///   - closure: The block of code to be executed
-    func down(to lower: Int, by step: Int = -1, _ closure: () -> Void) {
-
-        for _ in Swift.stride(from: self, through: lower, by: step).reversed() {
+    
+    func down(to lowerBound: Int, by step: Int = -1, _ closure: () -> Void) {
+        
+        for _ in Swift.stride(from: self, through: lowerBound, by: step).reversed() {
             closure()
         }
     }
@@ -131,29 +134,30 @@ public extension Int {
     /// - Parameter other: The denominator or divisor
     ///
     /// - Returns: True iff `self` is divisible, false otherwise
+    
     @discardableResult
     func isDivisible(by other: Int) -> Bool {
-
+        
         guard other != 0 else {
             return false
         }
-
+        
         return self % other == 0
     }
-
+    
     // MARK: - Type Methods
-
+    
     /// Generate a uniformly distributed random integer
     ///
     /// - Parameters:
     ///
-    ///   - lower: Inclusive lower bound value
-    ///   - upper: Inclusive upper bound value
+    ///   - lowerBound: Inclusive lower bound value
+    ///   - upperBound: Inclusive upper bound value
     ///
     /// - Returns: A random integer between `lower` and `upper` bounds
-    @discardableResult
-    static func random(from lower: Int, to upper: Int) -> Int {
 
-        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    @discardableResult
+    static func random(from lowerBound: Int, to upperBound: Int) -> Int {
+        return lowerBound + Int(arc4random_uniform(UInt32(upperBound - lowerBound + 1)))
     }
 }
--- Tests/IntTests.swift
+++ Tests/IntTests.swift
@@ -13,179 +13,179 @@ import XCTest
 import Shenanigans
 
 class IntTests: XCTestCase {
-
+    
     func testIsEven() {
-
+        
         for value in Swift.stride(from: -2, through: 4, by: 2) {
             XCTAssertTrue(value.isEven)
         }
-
+        
         for value in Swift.stride(from: -1, through: 7, by: 2) {
             XCTAssertFalse(value.isEven)
         }
     }
-
+    
     func testIsOdd() {
-
+        
         for value in Swift.stride(from: -2, through: 4, by: 2) {
             XCTAssertFalse(value.isOdd)
         }
-
+        
         for value in Swift.stride(from: -1, through: 7, by: 2) {
             XCTAssertTrue(value.isOdd)
         }
     }
-
+    
     func testIsPositive() {
-
+        
         for value in Swift.stride(from: 1, through: 4, by: 1) {
             XCTAssertTrue(value.isPositive)
         }
-
+        
         XCTAssertFalse(0.isPositive)
-
+        
         for value in Swift.stride(from: -7, through: -1, by: 2) {
             XCTAssertFalse(value.isPositive)
         }
     }
-
+    
     func testIsNegative() {
-
+        
         for value in Swift.stride(from: 1, through: 4, by: 1) {
             XCTAssertFalse(value.isNegative)
         }
-
+        
         XCTAssertFalse(0.isNegative)
-
+        
         for value in Swift.stride(from: -7, through: -1, by: 2) {
             XCTAssertTrue(value.isNegative)
         }
     }
-
+    
     func testIsNonPositive() {
-
+        
         for value in Swift.stride(from: 1, through: 4, by: 1) {
             XCTAssertFalse(value.isNonPositive)
         }
-
+        
         XCTAssertTrue(0.isNonPositive)
-
+        
         for value in Swift.stride(from: -7, through: -1, by: 2) {
             XCTAssertTrue(value.isNonPositive)
         }
     }
-
+    
     func testIsNonNegative() {
-
+        
         for value in Swift.stride(from: 1, through: 4, by: 1) {
             XCTAssertTrue(value.isNonNegative)
         }
-
+        
         XCTAssertTrue(0.isNonNegative)
-
+        
         for value in Swift.stride(from: -7, through: -1, by: 2) {
             XCTAssertFalse(value.isNonNegative)
         }
     }
-
+    
     func testIsNonZero() {
-
+        
         for value in Swift.stride(from: 1, through: 4, by: 1) {
             XCTAssertTrue(value.isNonZero)
         }
-
+        
         XCTAssertFalse(0.isNonZero)
     }
-
+    
     func testTimes() {
-
+        
         var value: Int
-
+        
         value = 0
         (-7).times { value += 1 }
         XCTAssertEqual(0, value)
-
+        
         value = 0
         0.times { value += 1 }
         XCTAssertEqual(0, value)
-
+        
         value = 0
         7.times { value += 1 }
         XCTAssertEqual(7, value)
-
+        
         value = 0
         (-7).times { value += $0 }
         XCTAssertEqual(0, value)
-
+        
         value = 0
         0.times { value += $0 }
         XCTAssertEqual(0, value)
-
+        
         value = 0
         7.times { value += $0 }
         XCTAssertEqual(21, value)
     }
-
+    
     func testUp() {
-
+        
         var value: Int
-
+        
         value = 0
         (-7).up(to: -1) { value += 1 }
         XCTAssertEqual(6, value)
-
+        
         value = 0
         0.up(to: 0) { value += 1 }
         XCTAssertEqual(0, value)
-
+        
         value = 0
         0.up(to: 7) { value += 1 }
         XCTAssertEqual(7, value)
     }
-
+    
     func testDown() {
-
+        
         var value: Int
-
+        
         value = 0
         (-1).down(to: -7) { value += 1 }
         XCTAssertEqual(7, value)
-
+        
         value = 0
         0.down(to: 0) { value += 1 }
         XCTAssertEqual(1, value)
-
+        
         value = 0
         7.down(to: 0) { value += 1 }
         XCTAssertEqual(8, value)
     }
-
+    
     func testIsDivisible() {
-
+        
         for value in Swift.stride(from: 1, through: 1, by: 1) {
             XCTAssertFalse(value.isDivisible(by: 0))
         }
-
+        
         for value in Swift.stride(from: -7, through: 7, by: 1) {
             XCTAssertTrue(value.isDivisible(by: 1))
         }
-
+        
         for value in Swift.stride(from: -2, through: 2, by: 2) {
             XCTAssertTrue(value.isDivisible(by: 2))
         }
     }
-
+    
     func testRandom() {
-
+        
         7.times {
             var value = Int.random(from: -7, to: 7)
-
+            
             XCTAssertGreaterThanOrEqual(value, -7)
             XCTAssertLessThanOrEqual(value, 7)
-
+            
             value = Int.random(from: 0, to: 0)
             XCTAssertEqual(value, 0)
-
+            
             value = Int.random(from: 0, to: 7)
             XCTAssertGreaterThanOrEqual(value, 0)
             XCTAssertLessThanOrEqual(value, 7)
