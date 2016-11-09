d4183b8d3339e9a414f8eef449bd8c6e17488b6b
Adriano Ferreira <adriano.ferreira@me.com>
2016-11-09 09:35:33 -0500

    Update collection extension and tests to swift 3

--- Sources/Collection.swift
+++ Sources/Collection.swift
@@ -9,46 +9,48 @@
 import Foundation
 
 public extension Collection {
-
+    
     // MARK: - Instance Methods
 
     /// Shuffle elements of `self`
     ///
     /// - Returns: A copy of `self` with its elements shuffled
+    
     @discardableResult
     func shuffled() -> [Iterator.Element] {
-
+        
         var array = Array(self)
         array.shuffle()
-
+        
         return array
     }
 }
 
 public extension Collection where IndexDistance == Int {
-
+    
     // MARK: - Instance Methods
-
+    
     /// Choose a random element from `self`
     ///
     /// - Returns: An optional random element from `self`, or `nil` if `self` is empty
+    
     @discardableResult
     func sampled() -> Iterator.Element? {
-
+        
         guard !isEmpty else {
             return nil
         }
-
+        
         let offset = Int.random(from: 0, to: count - 1)
-
+        
         return self[index(startIndex, offsetBy: offset)]
     }
 }
 
 public extension Collection where Index: Comparable {
-
+    
     // MARK: - Subscripts
-
+    
     /// Slice a collection by first clamping the range to its bounds
     ///
     /// - Parameter bounds: The range of values used to slice the collection
@@ -56,19 +58,22 @@ public extension Collection where Index: Comparable {
     /// - Returns: A bounded subsequence
     ///
     /// - Note: This behaviour avoids throwing a fatal error in execution time
+    
     subscript(clamping bounds: Range<Index>) -> SubSequence {
-
+        
         let clamped = bounds.clamped(to: startIndex ..< endIndex)
+        
         return self[clamped]
     }
-
+    
     subscript(clamping bounds: ClosedRange<Index>) -> SubSequence {
-
+        
         let range = bounds.lowerBound ..< index(bounds.upperBound, offsetBy: 1)
         let clamped = range.clamped(to: startIndex ..< endIndex)
+        
         return self[clamped]
     }
-
+    
     /// Slice a collection by first checking if a range is out of bounds
     ///
     /// - Parameter bounds: The range of values used to slice the collection
@@ -76,18 +81,21 @@ public extension Collection where Index: Comparable {
     /// - Returns: A bounded subsequence, or `nil` when the range is out of bounds
     ///
     /// - Note: This behaviour avoids throwing a fatal error in execution time
+    
     subscript(checking bounds: Range<Index>) -> SubSequence? {
-
+        
         let range = startIndex ... endIndex
+        
         return range.contains(bounds.lowerBound) && range.contains(bounds.upperBound) ? self[bounds] : nil
     }
-
+    
     subscript(checking bounds: ClosedRange<Index>) -> SubSequence? {
-
+        
         let range = bounds.lowerBound ..< index(bounds.upperBound, offsetBy: 1)
+        
         return self[checking: range]
     }
-
+    
     /// Slice a collection by first checking if index is out of bounds
     ///
     /// - Parameter index: The index used to slice the collection
@@ -95,12 +103,13 @@ public extension Collection where Index: Comparable {
     /// - Returns: An element of the collection, or `nil` iff it is out of bounds
     ///
     /// - Note: This behaviour avoids throwing a fatal error in execution time
+    
     subscript(checking index: Index) -> Iterator.Element? {
-
+        
         guard index >= startIndex && index < endIndex else {
             return nil
         }
-
+        
         return self[index]
     }
 }
--- Tests/CollectionTests.swift
+++ Tests/CollectionTests.swift
@@ -13,53 +13,53 @@ import XCTest
 import Shenanigans
 
 class CollectionTests: XCTestCase {
-
+    
     func testShuffled() {
-
+        
         let first = Array(-7 ... 7)
         let second = first.shuffled()
-
+        
         XCTAssertEqual(first.count, second.count)
-
+        
         for value in first {
             XCTAssertTrue(second.contains(value))
         }
-
+        
         XCTAssertEqual(first.sorted(), second.sorted())
     }
-
+    
     func testSampled() {
-
+        
         var first = [Int]()
         var second = first.sampled()
         XCTAssertNil(second)
-
+        
         first = Array(-7 ... 7)
         second = first.sampled()
         XCTAssertNotNil(second)
         XCTAssertTrue(first.contains(second!))
     }
-
+    
     func testSubscript() {
-
+        
         let array = [1, 2, 3]
-
+        
         XCTAssertEqual([1, 2, 3], array[clamping: 0 ..< 5])
         XCTAssertEqual([1, 2], array[clamping: -1 ..< 2])
         XCTAssertEqual([2], array[clamping: 1 ..< 2])
         XCTAssertEqual([], array[clamping: 3 ..< 4])
         XCTAssertEqual([3], array[clamping: 2 ... 4])
-
+        
         XCTAssertNil(array[checking: -2 ..< -1])
         XCTAssertNil(array[checking: -1 ..< 2])
         XCTAssertNil(array[checking: -1 ... 4])
         XCTAssertNil(array[checking: 2 ..< 5])
         XCTAssertNil(array[checking: 3 ... 5])
-
+        
         XCTAssertEqual([1, 2, 3], array[checking: 0 ..< 3]!)
         XCTAssertEqual([2, 3], array[checking: 1 ... 2]!)
         XCTAssertEqual([2], array[checking: 1 ..< 2]!)
-
+        
         XCTAssertNil(array[checking: -1])
         XCTAssertNil(array[checking: 3])
         XCTAssertEqual(2, array[checking: 1])
