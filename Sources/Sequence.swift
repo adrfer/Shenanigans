1fa4d564368bf0c40bcc53c61c578d1d61d990bf
Adriano Ferreira <adriano.ferreira@me.com>
2016-11-09 09:32:04 -0500

    Update sequence extension and tests to swift 3

--- Sources/Sequence.swift
+++ Sources/Sequence.swift
@@ -9,139 +9,155 @@
 import Foundation
 
 public extension Sequence {
-
+    
     // MARK: - Instance Methods
-
+    
     /// Drop elements of `self` that satisfy the given predicate
     ///
     /// - Parameter predicate: The predicate called on each element of `self`
     ///
     /// - Returns: A subsequence containing all elements of `self` but those the predicate was false
+    
     @discardableResult
     func drop(while predicate: (Iterator.Element) throws -> Bool) rethrows -> AnySequence<Iterator.Element> {
-
+        
         var array = Array(self)
         var generator = makeIterator()
-
+        
         while let element = generator.next() {
-
-            if try !predicate(element) { break }
+            
+            if try !predicate(element) {
+                break
+            }
+            
             array = Array(array.dropFirst())
         }
-
+        
         return AnySequence(array)
     }
-
+    
     /// Take elements of `self` that satisfy the given predicate
     ///
     /// - Parameter predicate: The predicate called on each element of `self`
     ///
     /// - Returns: A new array containing all elements of `self` for which the predicate was true
+    
     @discardableResult
     func take(while predicate: (Iterator.Element) throws -> Bool) rethrows -> AnySequence<Iterator.Element> {
-
+        
         var array = ContiguousArray<Iterator.Element>()
         var generator = makeIterator()
-
+        
         while let element = generator.next() {
-
-            if try !predicate(element) { break }
+            
+            if try !predicate(element) {
+                break
+            }
+            
             array.append(element)
         }
-
+        
         return AnySequence(array)
     }
-
+    
     /// Find the first element of `self` that satisfies the given predicate
     ///
     /// - Parameter predicate: The predicate called on each element of `self`
     ///
     /// - Returns: The first element where `predicate` is `true`, `nil` otherwise
+    
     @discardableResult
     func find(where predicate: (Iterator.Element) throws -> Bool) rethrows -> Iterator.Element? {
-
+        
         for element in self where try predicate(element) {
             return element
         }
-
+        
         return nil
     }
-
+    
     /// Check if at least one element of `self` satisfies the given predicate
     ///
     /// - Parameter predicate: The predicate called on each element of `self`
     ///
     /// - Returns: True iff any element in `self` satisfies `predicate`, false otherwise
+    
     @discardableResult
     func any(where predicate: (Iterator.Element) throws -> Bool) rethrows -> Bool {
-
+        
         for element in self where try predicate(element) {
             return true
         }
-
+        
         return false
     }
-
+    
     /// Check if all elements of `self` satisfy a given predicate the given predicate
     ///
     /// - Parameter predicate: The predicate called on each element of `self`
     ///
     /// - Returns: True iff every element in `self` satisfies `predicate`, false otherwise
+    
     @discardableResult
     func all(where predicate: (Iterator.Element) throws -> Bool) rethrows -> Bool {
-
+        
         for element in self where try !predicate(element) {
             return false
         }
-
+        
         return true
     }
-
+    
     /// Check if none elements of `self` satisfy a given predicate the given predicate
     ///
     /// - Parameter predicate: The predicate called on each element of `self`
     ///
     /// - Returns: True iff every element in `self` does not satisfy `predicate`, false otherwise
+    
     @discardableResult
     func none(where predicate: (Iterator.Element) throws -> Bool) rethrows -> Bool {
-
+        
         for element in self where try predicate(element) {
             return false
         }
-
+        
         return true
     }
 }
 
 public extension Sequence where Iterator.Element: Hashable {
-
+ 
+    // MARK: - Instance Methods
+    
     /// Get the frequencies of the elements of `self`
     ///
     /// - Returns: A dictionary where the keys are the elements of `self`, and the values are their frequencies
+    
     @discardableResult
     func frequencies() -> [Iterator.Element: Int] {
-
+        
         var dictionary = Dictionary<Iterator.Element, Int>()
-
+        
         for key in self {
             dictionary[key] = dictionary[key]?.advanced(by: 1) ?? 1
         }
-
+        
         return dictionary
     }
-
+    
     /// Get the unique elements of `self`
     ///
     /// - Returns: All unique elements of `self`
     ///
     /// - Note: Alternatively, one could pass the sequence to Set to remove duplicates
+    
     @discardableResult
     func unique() -> [Iterator.Element] {
-
+        
         var array = Set<Iterator.Element>()
-
+        
         return filter {
-
+            
             if array.contains($0) {
                 return false
             } else {
--- Tests/SequenceTests.swift
+++ Tests/SequenceTests.swift
@@ -13,111 +13,111 @@ import XCTest
 import Shenanigans
 
 class SequenceTests: XCTestCase {
-
+    
     func testDrop() {
-
+        
         var first = [Int]()
         var second = first.drop(while: { $0 > 7 })
         XCTAssertEqual(Array(second), [Int]())
-
+        
         first = Array(1 ... 7)
         second = first.drop(while: { $0 > 0 })
         XCTAssertEqual(Array(second), [Int]())
-
+        
         first = Array(-7 ... 7)
         second = first.drop(while: { $0 <= 0 })
         XCTAssertEqual(Array(second), Array(1 ... 7))
     }
-
+    
     func testTake() {
-
+        
         var first = [Int]()
         var second = first.take(while: { $0 > 7 })
         XCTAssertEqual(Array(second), [Int]())
-
+        
         first = Array(1 ... 7)
         second = first.take(while: { $0 < 0 })
         XCTAssertEqual(Array(second), [Int]())
-
+        
         first = Array(-7 ... 7)
         second = first.take(while: { $0 <= 0 })
         XCTAssertEqual(Array(second), Array(-7 ... 0))
     }
-
+    
     func testFind() {
-
+        
         var first = [Int]()
         var second = first.find(where: { $0 > 7})
         XCTAssertNil(second)
-
+        
         first = Array(-7 ... 7)
         second = first.find(where: { $0 < -7 })
         XCTAssertNil(second)
-
+        
         first = Array(0 ... 7)
         second = first.find(where: { $0 > 0 })
         XCTAssertEqual(second, 1)
     }
-
+    
     func testAny() {
-
+        
         var first = [Int]()
         var second = first.any(where: { $0 > 7 })
         XCTAssertFalse(second)
-
+        
         first = Array(1 ... 7)
         second = first.any(where: { $0.isNegative })
         XCTAssertFalse(second)
-
+        
         first = Array(-7 ... 7)
         second = first.any(where: { $0 < 0 })
         XCTAssertTrue(second)
     }
-
+    
     func testAll() {
-
+        
         var first = [Int]()
         var second = first.all(where: { $0 > 7 })
         XCTAssertTrue(second)
-
+        
         first = Array(1 ... 7)
         second = first.all(where: { $0 > 0 })
         XCTAssertTrue(second)
-
+        
         first = Array(-7 ... 7)
         second = first.all(where: { $0 < 0 })
         XCTAssertFalse(second)
     }
-
+    
     func testNone() {
-
+        
         var first = [Int]()
         var second = first.none(where: { $0 > 7 })
         XCTAssertTrue(second)
-
+        
         first = Array(1 ... 7)
         second = first.none(where: { $0.isPositive })
         XCTAssertFalse(second)
-
+        
         first = Array(-7 ... -1)
         second = first.none(where: { $0 > 0 })
         XCTAssertTrue(second)
     }
-
+    
     func testFrequencies() {
-
+        
         let first = [1, 1, 2, 3, 3, 7, 7, 7]
         XCTAssertEqual([1: 2, 2: 1, 3: 2, 7: 3], first.frequencies())
-
+        
         let second = ["A", "AB", "A", "ABC", "AB", "A"]
         XCTAssertEqual(["A": 3, "AB": 2, "ABC": 1], second.frequencies())
     }
-
+    
     func testUnique() {
-
+        
         let first = [1, 1, 2, 3, 3, 7, 7, 7]
         XCTAssertEqual([1, 2, 3, 7], first.unique())
-
+        
         let second = ["A", "AB", "A", "ABC", "AB", "A"]
         XCTAssertEqual(["A", "AB", "ABC"], second.unique())
     }
